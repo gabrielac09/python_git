ee9f3adb51f2757a9184c74f94c351fa16031cd3
Gabriela Contreras <gabrielac0901@gmail.com>
2026-05-19 00:50:59 -0300

    agregue hola.py

---
+++ hola.py
@@ -0,0 +1,11 @@
+print("hola, mundo")
+nombre = input("¿Cuál es tu nombre? ")
+print(f"¡Hola, {nombre}!, ahora te pedire unos datos")
+
+# Ley de Ohm
+V = float(input("Voltaje (V): "))
+R = float(input("Resistencia (Ohm): "))
+
+I = V / R
+
+print("Corriente =", I, "A")
