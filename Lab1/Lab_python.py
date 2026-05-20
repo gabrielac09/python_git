406b28e3632b2057b7d6f649f344d81cf82b5110
Gabriela Contreras <gabrielac0901@gmail.com>
2026-05-20 00:43:14 -0300

    Agregue control con comunicacion UART con comandos y comparacion de filtros

--- Lab1/Lab_python.py
+++ Lab1/Lab_python.py
@@ -15,31 +15,6 @@ ser.flushInput()
 ser.flushOutput()
 
 
-print('Ingrese un comando:\n')
-while 1 :
-    char_v = []
-    data = input("ToSent: ")
-    if data == 'exit':
-        if ser.isOpen():
-            ser.close()
-        break
-    else:
-        # Arma el vector a transmitir
-        for ptr in range(len(data)):
-            char_v.append(data[ptr])
-        print(char_v)
-        
-        for ptr in range(len(char_v)):
-            ser.write(char_v[ptr].encode())
-            #time.sleep(1)
-
-        out = ''
-        while ser.inWaiting() > 0:
-            out += ser.read(1).decode()
-
-        if out != '':
-            print(">> " + out)
-
 class RaisedCosineFilter:
     def __init__(self, alpha=0.25, span=6, sps=8, rrc=True):
         """
@@ -47,9 +22,15 @@ class RaisedCosineFilter:
         
         Parámetros:
         - alpha: Factor de roll-off (0 <= alpha <= 1)
-        - span: Duración total del filtro en símbolos
+        - span: Duración total del filtro en símbolos 
         - sps: Muestras por símbolo
         - rrc: Si True, genera raíz de coseno realzado; si False, coseno realzado
+        -> si quiero que el beta sea esto 1 estoy teniendo mucho ancho de banda y la señal es lo mas parecida a un pulso y seria lo ideal pero consume mucho ancho de banda
+        si cambio un poco el beta a 0.5 o 0.25 la señal se va a parecer un poco menos a un pulso pero consume menos ancho de banda
+         
+        -> si alpha es mas cercano a cero comprimo mas abrupta es la caida del filtro comprimo en frecuencia, expando en tiempo
+        -> cual es la resolucion que debo usar en cada coeficiente, cuantos coeficientes y la atenuacion maxima que necesitaria tener en base a la resolucion y cantidad de coeficientes
+        -> el filtro de RCC es una cuadrada con alpha cero, en cuanto mas lo relaj llevando el alfa a 1 se suavia mas y pasa a tener mas ancho de banda
         """
         self.alpha = alpha
         self.span = span
@@ -96,40 +77,168 @@ class RaisedCosineFilter:
 
         return h
 
-    def plot(self, time_domain=True, freq_domain=False):
+    def plot(self, time_domain=True, freq_domain=False, plot_type=1):
 
         t = np.arange(-len(self.taps)//2, len(self.taps)//2 + 1) / self.sps
+        plt.figure(figsize=(10,8))
 
         if time_domain:
-            plt.figure(figsize=(10, 4))
-            plt.plot(t[:len(self.taps)], self.taps, label="Impulse Response")
+            plt.subplot(2,1,1)
+            if plot_type == 0:   plt.stem(t[:len(self.taps)], self.taps, label="Impulse Response")
+            elif plot_type == 1: plt.plot(t[:len(self.taps)], self.taps, label="Impulse Response")
             plt.title("Raised Cosine Filter (Time Domain)")
             plt.xlabel("Time [symbol periods]")
             plt.ylabel("Amplitude")
             plt.grid(True)
             plt.legend()
-            plt.tight_layout()
-            plt.show(block=False)
 
         if freq_domain:
             H = np.fft.fftshift(np.fft.fft(self.taps, 1024))
             f = np.linspace(-0.5, 0.5, len(H), endpoint=False)
-            plt.figure(figsize=(10, 4))
-            plt.plot(f, 20 * np.log10(np.abs(H) + 1e-6), label="Frequency Response [dB]")
+            plt.subplot(2,1,2)
+            if plot_type == 0:   plt.stem(f, 20 * np.log10(np.abs(H) + 1e-6), label="Frequency Response [dB]")
+            elif plot_type == 1: plt.plot(f, 20 * np.log10(np.abs(H) + 1e-6), label="Frequency Response [dB]")
             plt.title("Raised Cosine Filter (Frequency Domain)")
             plt.xlabel("Normalized Frequency [×π rad/sample]")
             plt.ylabel("Magnitude [dB]")
             plt.grid(True)
             plt.legend()
-            plt.tight_layout()
-            plt.show()
 
+        plt.tight_layout()
+        plt.show()
+        
     def get_coefficients(self):
         return self.taps
+
+"""
 filtro = RaisedCosineFilter(alpha=1, span=10, sps=10, rrc=True)
 
 coef = filtro.get_coefficients()
 print(coef)
 
 filtro.plot(time_domain=True, freq_domain=True)
+"""
 
+# Variables iniciales del filtro
+# alpha -> rolloff
+# span  -> cantidad de símbolos
+# sps   -> muestras por símbolo
+# rrc   -> True = Root Raised Cosine
+
+alpha = 1
+span = 10
+sps = 10
+rrc = True
+plot_type = 1
+saved_filters = []
+comm= ""
+comment = ""
+
+
+print("\nComandos disponibles:")
+print("alpha=0.5")
+print("span=8")
+print("sps=16")
+print("rrc=0")
+print("plot=0")
+print("plot=1")
+print("plot")
+print("save")
+print("comment=Comentario del filtro")
+print("reset")
+print("compare")
+print("coef")
+print("exit\n")
+
+
+
+
+
+
+
+while 1 :
+    data = input("ToSent: ")
+    if data == 'exit':
+        if ser.isOpen():
+            ser.close()
+        break
+    else:
+        ser.write(data.encode())
+        out = ''
+        while ser.inWaiting() > 0:
+            out += ser.read(1).decode()
+        #print(">> " + out)
+
+        if "alpha=" in out:
+            alpha = float(out.split('=')[1])
+            print(f"Nuevo alpha: {alpha}")
+        elif "span=" in out:
+            span = int(out.split('=')[1])
+            print(f"Nuevo span: {span}")
+        elif "sps=" in out:
+            sps = int(out.split('=')[1])
+            print(f"Nuevo sps: {sps}")
+        elif "rrc=" in out:
+            rrc = bool(int(out.split('=')[1]))
+            print(f"Nuevo rrc: {rrc}")
+        elif "plot=" in out:
+            plot_type = int(out.split('=')[1])
+            filtro.plot(time_domain=True, freq_domain=True, plot_type=plot_type)
+        elif "comment=" in out:
+            comment = out.split("=")[1]
+            print(f"Nuevo comentario: {comment}")
+        
+        
+        filtro= RaisedCosineFilter(alpha=alpha, span=span, sps=sps, rrc=rrc)
+
+    
+        if out == "plot":
+            filtro.plot(time_domain=True, freq_domain=True, plot_type=plot_type)
+        
+        if out == "save":
+            saved_filters.append((filtro, comment))
+            comment = ""
+            print(f"Filtro guardado: Total guardados: {len(saved_filters)}")
+
+        if out == "compare":
+            if len(saved_filters) < 2:
+                print("Se necesitan al menos 2 filtros guardados para comparar.")
+            else:
+                plt.figure(figsize=(10, 8))
+
+                plt.subplot(2,1,1)
+                for idx, (f,comm) in enumerate(saved_filters):
+                    t = np.arange(-len(f.taps)//2, len(f.taps)//2 + 1) / f.sps
+                    if plot_type == 0:   plt.stem(t[:len(f.taps)], f.taps, label=f"Filtro {idx+1} -{comm}")
+                    elif plot_type == 1: plt.plot(t[:len(f.taps)], f.taps, label=f"Filtro {idx+1} -{comm}")
+                plt.title("Comparacion Temporal de Filtros")
+                plt.xlabel("Tiempo [símbolos]")
+                plt.ylabel("Amplitud")
+                plt.grid(True)
+                plt.legend()
+                
+
+                plt.subplot(2,1,2)
+                for idx, (f,comm) in enumerate(saved_filters):
+                    H = np.fft.fftshift(np.fft.fft(f.taps, 1024))
+                    f_axis = np.linspace(-0.5, 0.5, len(H), endpoint=False)
+                    if plot_type == 0:   plt.stem(f_axis, 20 * np.log10(np.abs(H) + 1e-6), label=f"Filtro {idx+1} -{comm}")
+                    elif plot_type == 1: plt.plot(f_axis, 20 * np.log10(np.abs(H) + 1e-6), label=f"Filtro {idx+1} -{comm}")
+                plt.title("Comparacion en Frecuencia de Filtros")
+                plt.xlabel("Frecuencia Normalizada [×π rad/sample]")
+                plt.ylabel("Magnitud [dB]")
+                plt.grid(True)
+                plt.legend()
+                plt.figtext(0.05, 0.01, f"Comentario: {comment}", ha="left", fontsize=10, color="gray")
+
+                plt.tight_layout()
+                plt.show()
+
+        if out == "coef":
+            print(filtro.get_coefficients())
+        
+        if out == "reset":
+            saved_filters.clear()
+            print("Filtros guardados eliminados.")
+
+        
